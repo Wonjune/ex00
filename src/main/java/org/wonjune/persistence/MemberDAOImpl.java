723c51035615e1ece44a3e5e061dafb390b50d3d
이원준 <cloudysoul@naver.com>
2016-05-08 23:16:35 +0900

    JDBC 로그상세 출력 추가

---
+++ src/main/java/org/wonjune/persistence/MemberDAOImpl.java
@@ -0,0 +1,28 @@
+package org.wonjune.persistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+import org.wonjune.domain.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String namespace = "org.wonjune.mapper.MemberMapper";
+	
+	@Override
+	public String getTime() {
+		return sqlSession.selectOne(namespace + ".getTime");
+	}
+
+	@Override
+	public void insertMember(MemberVO vo) {
+		sqlSession.insert(namespace + ".insertMember", vo);
+		
+	}
+	
+}
